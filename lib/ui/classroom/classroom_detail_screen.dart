fe4ca0f6fa74d65c6d72320c17c35bb742cba659
ramdanfegno <108392413+ramdanfegno@users.noreply.github.com>
2023-12-30 00:59:41 +0530

    friday 30 dec 12:59 am

--- lib/ui/classroom/classroom_detail_screen.dart
+++ lib/ui/classroom/classroom_detail_screen.dart
@@ -1,4 +1,5 @@
-import 'package:edu_app/data/models/classroom_model.dart';
+import 'dart:developer';
+
 import 'package:edu_app/data/models/classroom_model.dart';
 import 'package:edu_app/ui/custom%20widgets/custom_app_bar.dart';
 import 'package:edu_app/utils/color_constants.dart';
@@ -20,11 +21,16 @@ class ClassroomDetailScreen extends StatefulWidget {
 
 class _ClassroomDetailScreenState extends State<ClassroomDetailScreen> {
   ClassroomBloc? classroomBloc;
+  int? topLayer, bottomLayer;
+  double? tableHeight;
 
   @override
   void initState() {
     classroomBloc = ClassroomBloc();
     classroomBloc!.add(GetClassDetailEvent(id: widget.classroom.id!));
+    topLayer = 0;
+    bottomLayer = 0;
+    tableHeight = 0.0;
     super.initState();
   }
 
@@ -33,9 +39,10 @@ class _ClassroomDetailScreenState extends State<ClassroomDetailScreen> {
     return BlocProvider(
       create: (context) => classroomBloc!,
       child: Scaffold(
-        appBar: const CustomAppBar(
-          title: "Classroom Detail",
+        appBar: CustomAppBar(
+          title: widget.classroom.name!,
           homeBar: false,
+          actionIcon: optionsTile(),
         ),
         body: SafeArea(
           child: Center(
@@ -48,7 +55,7 @@ class _ClassroomDetailScreenState extends State<ClassroomDetailScreen> {
                     height: SizeConfig.blockSizeHorizontal * 5,
                   ),
                   const Text(
-                    'This way up',
+                    'Faces this way',
                     style: TextStyle(
                         color: ClrConst.primaryGrey,
                         fontSize: 18,
@@ -63,10 +70,14 @@ class _ClassroomDetailScreenState extends State<ClassroomDetailScreen> {
                     decoration: BoxDecoration(
                         color: ClrConst.primaryDeepBlue.withOpacity(0.2),
                         borderRadius: BorderRadius.circular(12)),
-                    child: const Center(
-                        child: Text(
-                      'T',
-                      style: TextStyle(fontFamily: Fonts.poppins, fontSize: 24),
+                    child: Center(
+                        child: Transform.rotate(
+                      angle: 1.6,
+                      child: const Text(
+                        'T',
+                        style:
+                            TextStyle(fontFamily: Fonts.poppins, fontSize: 24),
+                      ),
                     )),
                   ),
                   SizedBox(
@@ -93,21 +104,71 @@ class _ClassroomDetailScreenState extends State<ClassroomDetailScreen> {
                       }
 
                       if (state is ClassroomDetailLoadedState) {
-                        return GridView.builder(
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 4,
-                              childAspectRatio: 1,
-                              // Set the number of columns as needed
-                              crossAxisSpacing:
-                                  SizeConfig.blockSizeHorizontal * 4,
-                              mainAxisSpacing:
-                                  SizeConfig.blockSizeHorizontal * 4,
+                        if (state.classroomDetailModel.layout == 'classroom') {
+                          return GridView.builder(
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 4,
+                                childAspectRatio: 1,
+                                // Set the number of columns as needed
+                                crossAxisSpacing:
+                                    SizeConfig.blockSizeHorizontal * 2,
+                                mainAxisSpacing:
+                                    SizeConfig.blockSizeHorizontal * 2,
+                              ),
+                              itemCount: state.classroomDetailModel.size,
+                              itemBuilder: (BuildContext context, int index) {
+                                return studentTile("${index + 1}");
+                              });
+                        } else if (state.classroomDetailModel.layout ==
+                            'conference') {
+                          bottomLayer = state.classroomDetailModel.size! ~/ 2;
+
+                          topLayer =
+                              state.classroomDetailModel.size! - bottomLayer!;
+                          tableHeight = SizeConfig.blockSizeHorizontal *
+                              17 *
+                              (topLayer! > bottomLayer!
+                                  ? topLayer!
+                                  : bottomLayer!);
+
+                          return SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        for (int i = 0; i < bottomLayer!; i++)
+                                          studentTile("${i + 1 + topLayer!}"),
+                                      ],
+                                    ),
+                                    Container(
+                                      height: tableHeight,
+                                      width:
+                                          SizeConfig.blockSizeHorizontal * 40,
+                                      decoration: BoxDecoration(
+                                          color: ClrConst.primaryDeepBlue
+                                              .withOpacity(0.1),
+                                          borderRadius:
+                                              BorderRadius.circular(12)),
+                                      child: const Center(),
+                                    ),
+                                    Column(
+                                      children: [
+                                        for (int i = 0; i < topLayer!; i++)
+                                          studentTile("${i + 1}"),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
-                            itemCount: state.classroomDetailModel.size,
-                            itemBuilder: (BuildContext context, int index) {
-                              return studentTile("${index + 1}".toString());
-                            });
+                          );
+                        }
                       }
                       return Container();
                     },
@@ -121,18 +182,40 @@ class _ClassroomDetailScreenState extends State<ClassroomDetailScreen> {
     );
   }
 
-  Widget studentTile(String index) {
-    return Container(
-      height: SizeConfig.blockSizeHorizontal * 15,
-      width: SizeConfig.blockSizeHorizontal * 15,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: ClrConst.primaryDeepBlue.withOpacity(0.2)),
-      child: Center(
-          child: Text(
-        "S$index",
-        style: TextStyle(fontFamily: Fonts.poppins, fontSize: 24),
-      )),
+  Widget studentTile(String number) {
+    return Padding(
+      padding: EdgeInsets.all(SizeConfig.blockSizeHorizontal * 1),
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: ClrConst.primaryDeepBlue.withOpacity(0.2)),
+        child: Padding(
+          padding: EdgeInsets.all(SizeConfig.blockSizeHorizontal * 4),
+          child: Center(
+              child: Transform.rotate(
+            angle: 1.58,
+            child: Text(
+              "S$number",
+              style: const TextStyle(fontFamily: Fonts.poppins, fontSize: 24),
+            ),
+          )),
+        ),
+      ),
+    );
+  }
+
+  Widget optionsTile() {
+    return PopupMenuButton(
+      itemBuilder: (context) {
+        return [
+          const PopupMenuItem(value: 0, child: Text('Change/Add subject'))
+        ];
+      },
+      onSelected: (val) {
+        if (val == 0) {
+          log('Change/Add subject');
+        }
+      },
     );
   }
 }
